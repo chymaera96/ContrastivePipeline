1382f554031df25b7a1376969b5dd26e2b80817d
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-07-11 12:27:30 +0100

    reduced aug probability

--- sfnet/gpu_transformations.py
+++ sfnet/gpu_transformations.py
@@ -23,11 +23,11 @@ class GPUTransformNeuralfp(nn.Module):
         #     ])
         
         self.cpu_transform = Compose([
-            ApplyImpulseResponse(ir_paths=self.ir_dir, p=0.7),
+            ApplyImpulseResponse(ir_paths=self.ir_dir, p=0.5),
             AddBackgroundNoise(background_paths=self.noise_dir, 
                                min_snr_in_db=cfg['tr_snr'][0],
                                max_snr_in_db=cfg['tr_snr'][1], 
-                               p=0.7),
+                               p=0.5),
             ])
         
         self.val_transform = Compose([
