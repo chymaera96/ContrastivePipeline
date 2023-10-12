63c7832ed27ecc39e76603cb2035c293285b165e
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-10-13 00:40:23 +0100

    divide-and-encode

--- sfnet/modules/simclr.py
+++ sfnet/modules/simclr.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 d = 128
-h = 2304
+h = 1152
 u = 16
 v = int(h/d)
 
@@ -11,25 +11,26 @@ class SimCLR(nn.Module):
     def __init__(self, encoder):
         super(SimCLR, self).__init__()
         self.encoder = encoder
-        self.projector = nn.Sequential(nn.Linear(v,u),
-                                       nn.ELU(),
-                                       nn.Linear(u,1)
-                                       )
-
+        # self.projector = nn.Sequential(nn.Linear(v,u),
+        #                                nn.ELU(),
+        #                                nn.Linear(u,1)
+        #                                )
+        self.projector = nn.Sequential(nn.Conv1d(h, d * u, kernel_size=(1,), groups=d),
+                                        nn.ELU(),
+                                        nn.Conv1d(d * u, d, kernel_size=(1,), groups=d)
+                                        )
 
     def forward(self, x_i, x_j):
         
+
         h_i = self.encoder(x_i)
         print(f'Shape of h_i {h_i.shape} inside the SimCLR forward function')
-        new_h_i = h_i.view(h_i.size(0), -1, v)
-        z_i = self.projector(new_h_i).squeeze(-1)
-        z_i = F.normalize(z_i, p=2)
+        z_i = self.projector(h_i.unsqueeze(-1)).squeeze(-1)
         print(f'Shape of z_i {z_i.shape} inside the SimCLR forward function')
-
+        z_i = F.normalize(z_i, p=2)
 
         h_j = self.encoder(x_j)
-        new_h_j = h_j.view(h_j.size(0), -1, v)
-        z_j = self.projector(new_h_j).squeeze(-1)
+        z_j = self.projector(h_j.unsqueeze(-1)).squeeze(-1)
         z_j = F.normalize(z_j, p=2)
 
 
