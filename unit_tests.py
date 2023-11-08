9f3bd2855762210003d7b6ad3dd46571b476ac69
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-11-08 15:40:44 +0000

    correcting unit test

--- unit_tests.py
+++ unit_tests.py
@@ -38,7 +38,7 @@ class MoCoEncoderTestCase(unittest.TestCase):
         x_ptrs = set(e.data_ptr() for e in x.view(-1))
         y_ptrs = set(e.data_ptr() for e in y.view(-1))
         flag = (x_ptrs < y_ptrs) or (y_ptrs < x_ptrs)
-        self.assertTrue(flag, 'The parameters of the two encoders are not the same')
+        self.assertTrue(flag, 'The parameters of the two encoders are the same')
 
 if __name__ == '__main__':
     unittest.main()
