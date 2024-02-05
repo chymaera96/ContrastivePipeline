66eb10a6ed63aa8f440d08fc9e43d1ca8ab561b0
Aditya <a.bhattacharjee@qmul.ac.uk>
2024-02-05 19:23:54 +0000

    unit test: is test split good?

--- util.py
+++ util.py
@@ -206,16 +206,26 @@ def get_test_index(data_dir):
     return test_idx
     
 def main():
-    test_noise_dir = '/import/c4dm-datasets/FSD50K/dev_audio'
-    for label in ['speech', 'vehicle', 'farm', 'home']:
-        noise_train_idx = load_augmentation_index(test_noise_dir, json_path=label+'.json', splits=1.0)
-        # Check if files in index are in the directory
-        for fpath in noise_train_idx:
-            if not os.path.exists(fpath):
-                print(f"File {fpath} not found. Removing from index")
-                noise_train_idx.remove(fpath)
-        with open(label+'.json', 'w') as fp:
-            json.dump(noise_train_idx, fp)
+    # test_noise_dir = '/import/c4dm-datasets/FSD50K/dev_audio'
+    # for label in ['speech', 'vehicle', 'farm', 'home']:
+    #     noise_train_idx = load_augmentation_index(test_noise_dir, json_path=label+'.json', splits=1.0)
+    #     # Check if files in index are in the directory
+    #     for fpath in noise_train_idx:
+    #         if not os.path.exists(fpath):
+    #             print(f"File {fpath} not found. Removing from index")
+    #             noise_train_idx.remove(fpath)
+    #     with open(label+'.json', 'w') as fp:
+    #         json.dump(noise_train_idx, fp)
+
+    with open('data/test_idx.json', 'r') as fp:
+        test = json.load(fp)
+
+    with open('data/fma_medium.json', 'r') as fp:
+        train = json.load(fp)
+    
+    for k,v in test.items():
+        if v in train.values():
+            print(f"File {v} found in train index")
 
 
 if __name__ == '__main__':
